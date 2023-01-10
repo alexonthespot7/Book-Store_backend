ba485a1c4e1a473cfd9391eaeea1b2ffece5be3a
unknown <bgw595@myy.haaga-helia.fi>
2023-01-11 00:54:41 +0200

    fourht try

--- src/main/java/com/pro/mybooklist/model/Category.java
+++ src/main/java/com/pro/mybooklist/model/Category.java
@@ -4,12 +4,12 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Category {
